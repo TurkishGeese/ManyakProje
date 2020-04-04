950020e7c96b0bbbf47b7cc1cd4d27f24a005f24
boraozarslan <borako.ozarslan@gmail.com>
2020-04-04 16:20:56 -0400

    Skeleton code for the input manager

---
+++ src/inputManager.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "manyakSDL.hpp"
+#include <map>
+
+#include "inputState.hpp"
+#include "inputType.hpp"
+#include "keyboardInput.hpp"
+
+class InputManager {
+
+public:
+	static void initialize();
+	static void quit();
+	static void reset();
+
+	static void registerObject(GameObject* obj, InputType inputType);
+	static void updateInput();
+	static InputState getInputState(SDL_Keycode key); // This function should only be called by the level
+	// TODO there should be a higher level of abstraction on objects called an Object. The below function should be able to take UIObjects as well.
+	static InputState getInputState(GameObject* obj, SDL_Keycode key); // This function should only be called by objects
+
+private:
+	InputManager();
+
+	void internalReset();
+
+	void internalRegisterObject(GameObject* obj, InputType inputType);
+	void internalUpdateInput();
+	InputState internalGetInputState(SDL_Keycode key);
+	InputState internalGetInputState(GameObject* obj, SDL_Keycode key);
+
+	static InputManager* sInstance;
+
+	KeyboardInput mKeyboard;
+	std::map<GameObject*, Input*> mInputMapping = std::map<GameObject*, Input*>();
+};
