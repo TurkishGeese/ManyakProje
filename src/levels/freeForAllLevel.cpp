51e2308e1f99e14fd4dfaeb72d90742af66c8414
boraozarslan <borako.ozarslan@gmail.com>
2020-04-04 17:10:33 -0400

    Use the input manager

--- src/levels/freeForAllLevel.cpp
+++ src/levels/freeForAllLevel.cpp
@@ -2,10 +2,13 @@
 
 #include "player.hpp"
 #include "uiText.hpp"
+#include "inputManager.hpp"
 
 FreeForAllLevel::FreeForAllLevel() {
 	mGameObjects = std::vector<GameObject*>(1);
 	mUiObjects = std::vector<UIObject*>(1);
     mGameObjects[0] = new Player();
     mUiObjects [0] = new UIText("Bora", 0, 0, {255, 0, 0});
+
+	InputManager::registerObject(mGameObjects[0], KEYBOARD);
 }
