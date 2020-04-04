15532bbd550e838ee11c84ef506bf09ebdc04137
boraozarslan <borako.ozarslan@gmail.com>
2020-04-04 17:16:04 -0400

    Fix access to deleted iterator location bug

--- src/keyboardInput.cpp
+++ src/keyboardInput.cpp
@@ -12,18 +12,22 @@ InputState KeyboardInput::getInputState(SDL_Keycode key) {
 void KeyboardInput::updateInput() {
 	// If the key was pressed earlier, now it's being held.
 	// If the key was released earlier, now it has no state.
-	for (const auto& it : mInputStates)
+	for (std::map<SDL_Keycode, InputState>::iterator it = mInputStates.begin(); it != mInputStates.end();)
 	{
-		if (it.second == PRESSED) {
-			mInputStates[it.first] = HELD;
+		// Need to get next now because we might delete the current iterator
+		auto curIt = it;
+		it++;
+
+		if (curIt->second == PRESSED) {
+			mInputStates[curIt->first] = HELD;
 		}
-		else if (it.second == RELEASED) {
+		else if (curIt->second == RELEASED) {
 			// XXX: I'm not sure what's the best way of handling this here. If we remove an item the map internal functions `might` be slow.
 			// So we wouldn't want to do this. However, the other option is to update the key's value to a NONE. But in that case it'll always stay
 			// inside the mapping. At each frame we loop all values inside the map. Now we have an extra key to check every frame. My guy tells me
 			// that erasing should be better. However, we should really evaluate this. Perhaps there is no difference. I think this points to me
 			// not storing my data correctly. So ideally we should store these states differently.
-			mInputStates.erase(it.first);
+			mInputStates.erase(curIt->first);
 		}
 	}
 
